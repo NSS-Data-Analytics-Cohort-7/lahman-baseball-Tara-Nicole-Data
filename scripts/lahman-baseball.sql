760dc30b38a83b19c9aeb084b64bbede1f576688
Tara-Nicole-Data <tara.nicole.data@gmail.com>
2022-08-23 20:23:04 -0500

    Working 2b

--- scripts/lahman-baseball.sql
+++ scripts/lahman-baseball.sql
@@ -14,6 +14,21 @@ FROM homegames;
 
 --Question 1 Answer: 1871-2016
 
+/*Question 2a: Find the name and height of the shortest player in the database.*/
+
+SELECT 
+    namefirst,
+    namelast,
+    MIN (height)
+FROM people
+GROUP BY namefirst, namelast
+LIMIT 1; 
+
+--Question 2a Answer: Ned Harris, 71 inches or 5 feet 9 inches
+
+/*Question 2b: How many games did he play in?*/
+
+
 
 
  
