54c47be1682448ddc10e3ae3721bdb297f3db7d6
Tara-Nicole-Data <tara.nicole.data@gmail.com>
2022-08-23 21:15:50 -0500

    Starting 4

--- scripts/lahman-baseball.sql
+++ scripts/lahman-baseball.sql
@@ -27,7 +27,10 @@ ORDER BY height;
 
 /*Question 2b: How many games did he play in?*/
 
-SELECT p.namefirst, p.namelast, a.g_all
+SELECT 
+    p.namefirst, 
+    p.namelast, 
+    a.g_all
 FROM appearances AS a
 LEFT JOIN people AS p
 ON a.playerid = p.playerid
@@ -38,7 +41,10 @@ ORDER BY p.height;
 
 /*Question 2c: What is the name of the team for which he played?*/
 
-SELECT DISTINCT(t.name), p.namefirst, p.namelast
+SELECT 
+    DISTINCT(t.name), 
+    p.namefirst, 
+    p.namelast
 FROM appearances AS a
 LEFT JOIN people AS p
 ON a.playerid = p.playerid
@@ -48,6 +54,29 @@ WHERE namefirst = 'Eddie' AND namelast = 'Gaedel';
 
 --Question 2c Answer: St. Louis Browns
 
- 
+/*Question 3: Find all the players in the database who played at Vanderbilt University. Create a list showing each player's first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?*/
+
+SELECT 
+    s.schoolname, 
+    p.namefirst,
+    p.namelast,
+    SUM(salary) AS total_salary
+FROM schools AS s
+LEFT JOIN collegeplaying AS c
+    ON s.schoolid = c.schoolid
+LEFT JOIN people AS p
+    ON p.playerid = c.playerid
+LEFT JOIN salaries AS s2
+    ON p.playerid = s2.playerid
+WHERE schoolname = 'Vanderbilt University' 
+GROUP BY 
+    s.schoolname, 
+    p.namefirst, 
+    p.namelast
+ORDER BY total_salary DESC;
+
+--Question 3 Answer: David Price, $245,553,888
+
+
 
 
