35d85dc1e64a2ffa548ba0562cd59b52f433c5ac
Tara-Nicole-Data <tara.nicole.data@gmail.com>
2022-08-23 21:32:41 -0500

    Starting 5

--- scripts/lahman-baseball.sql
+++ scripts/lahman-baseball.sql
@@ -60,7 +60,7 @@ SELECT
     s.schoolname, 
     p.namefirst,
     p.namelast,
-    SUM(salary) AS total_salary
+    CAST(CAST(SUM(salary) AS NUMERIC) AS MONEY) --s/o to Sarah
 FROM schools AS s
 LEFT JOIN collegeplaying AS c
     ON s.schoolid = c.schoolid
@@ -73,10 +73,27 @@ GROUP BY
     s.schoolname, 
     p.namefirst, 
     p.namelast
-ORDER BY total_salary DESC;
+ORDER BY SUM(salary) DESC;
 
 --Question 3 Answer: David Price, $245,553,888
 
+/*Question 4: Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS","1B","2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.*/
 
+SELECT
+    SUM(po) AS total_putouts,
+    CASE WHEN pos = 'OF' THEN 'Outfield'
+         WHEN pos = 'SS' THEN 'Infield'
+         WHEN pos = '1B' THEN 'Infield'
+         WHEN pos = '2B' THEN 'Infield'
+         WHEN pos = '3B' THEN 'Infield'
+         WHEN pos = 'P' THEN 'Battery'
+         WHEN pos = 'C' THEN 'Battery'
+         END AS grouped_positions
+FROM fielding
+WHERE yearid = '2016'
+GROUP BY grouped_positions
+ORDER BY total_putouts DESC; 
+
+--Question 4 Answer: Infield-58,934; Battery-41,424; Outfield-29,560
 
 
