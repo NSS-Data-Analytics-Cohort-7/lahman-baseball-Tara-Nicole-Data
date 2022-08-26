7fe6f71f89174276d2bf00cbe93561708933055d
Tara-Nicole-Data <tara.nicole.data@gmail.com>
2022-08-25 20:05:40 -0500

    Working on 5

--- scripts/lahman-baseball.sql
+++ scripts/lahman-baseball.sql
@@ -57,23 +57,42 @@ WHERE namefirst = 'Eddie' AND namelast = 'Gaedel';
 /*Question 3: Find all the players in the database who played at Vanderbilt University. Create a list showing each player's first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?*/
 
 SELECT 
-    s.schoolname, 
+    c.schoolid, 
     p.namefirst,
     p.namelast,
-    CAST(CAST(SUM(salary) AS NUMERIC) AS MONEY) --s/o to Sarah
-FROM schools AS s
-LEFT JOIN collegeplaying AS c
-    ON s.schoolid = c.schoolid
-LEFT JOIN people AS p
-    ON p.playerid = c.playerid
-LEFT JOIN salaries AS s2
-    ON p.playerid = s2.playerid
-WHERE schoolname = 'Vanderbilt University' 
-GROUP BY 
-    s.schoolname, 
+    CAST(CAST(SUM(DISTINCT s.salary) AS NUMERIC) AS MONEY) AS salary--s/o to Sarah
+FROM collegeplaying AS c
+JOIN people AS p
+    USING (playerid)
+JOIN salaries AS s
+    USING (playerid)
+WHERE schoolid = 'vandy' AND p.namelast = 'Price'
+GROUP BY  
     p.namefirst, 
-    p.namelast
-ORDER BY SUM(salary) DESC;
+    p.namelast, 
+    c.schoolid;
+
+
+/*FROM TYLER: SELECT CONCAT(vandy.namefirst, ' ', vandy.namelast) AS name,
+      CAST(CAST(SUM(s.salary) AS numeric) AS money) AS player_pay
+    FROM (SELECT p.playerid,
+            p.namefirst,
+            p.namelast
+          FROM people AS p
+          LEFT JOIN collegeplaying AS cp
+          USING (playerid)
+          JOIN schools AS s
+          USING (schoolid)
+          WHERE s.schoolname LIKE '%Vanderbilt%'
+          GROUP BY p.playerid) AS vandy
+    JOIN salaries AS s
+    ON vandy.playerid = s.playerid
+    WHERE s.salary IS NOT null
+    GROUP BY vandy.namefirst,
+      vandy.namelast
+    ORDER BY player_pay DESC;*/
+
+
 
 --Question 3 Answer: David Price, $245,553,888
 
@@ -96,4 +115,12 @@ ORDER BY total_putouts DESC;
 
 --Question 4 Answer: Infield-58,934; Battery-41,424; Outfield-29,560
 
+/*Question 5: Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?*/
 
+SELECT ROUND(AVG(so),2), (yearid/10)*10 AS decade
+FROM pitching
+GROUP BY decade
+HAVING ((yearid/10)*10) >= '1920'
+ORDER BY decade DESC;
+
+ 
